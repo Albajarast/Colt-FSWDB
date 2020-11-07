013d375d3939bba285a2e8a904d428be5752b2a8
David López Albajara <dlalbajara@gmail.com>
2020-11-07 13:33:37 +0100

    Add third button and 'twist and shout' methods

--- Javascript/DOM/DOM_events/app.js
+++ Javascript/DOM/DOM_events/app.js
@@ -11,3 +11,34 @@ function scream() {
 }
 
 secondBtn.onmouseenter = scream;
+
+const thirdBtn = document.querySelector("#thirdBtn");
+thirdBtn.addEventListener("click", function () {
+  alert("Clicked!");
+});
+
+function twist() {
+  console.log("TWIST!");
+}
+
+function shout() {
+  console.log("SHOUT!");
+}
+
+const twistAndShout = document.querySelector("#twistAndShout");
+
+twistAndShout.addEventListener("click", twist);
+twistAndShout.addEventListener("click", shout);
+
+// Click events Exercise
+
+const hello = document.querySelector("#hello");
+const goodbye = document.querySelector("#goodbye");
+
+hello.addEventListener("click", function () {
+  console.log("hello");
+});
+
+goodbye.addEventListener("click", function () {
+  console.log("goodbye");
+});
