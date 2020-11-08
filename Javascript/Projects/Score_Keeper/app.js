0530bec7de5ea759fe3558f8cc9c2c809eb306e4
David López Albajara <dlalbajara@gmail.com>
2020-11-08 18:02:29 +0100

    Refactor to include players as objects with attributes for score, scoreDisplay and button

--- Javascript/Projects/Score_Keeper/app.js
+++ Javascript/Projects/Score_Keeper/app.js
@@ -1,63 +1,59 @@
-const buttonP1 = document.querySelector("#buttonP1");
-const buttonP2 = document.querySelector("#buttonP2");
+const p1 = {
+  score: 0,
+  button: document.querySelector("#buttonP1"),
+  scoreDisplay: document.querySelector("#scoreP1"),
+};
+
+const p2 = {
+  score: 0,
+  button: document.querySelector("#buttonP2"),
+  scoreDisplay: document.querySelector("#scoreP2"),
+};
+
 const reset = document.querySelector("#reset");
-const scoreDisplayP1 = document.querySelector("#scoreP1");
-const scoreDisplayP2 = document.querySelector("#scoreP2");
-const scoreLimit = document.querySelector("#games");
-const games = document.querySelector("#games");
+const gamesSelector = document.querySelector("#games");
 
-let scoreP1 = 0;
-let scoreP2 = 0;
-let winningScore = 6;
+let winningScore = parseInt(gamesSelector.value);
+let isGameOver = false;
 
-buttonP1.addEventListener("click", function () {
-  scoreP1++;
-  scoreDisplayP1.innerText = scoreP1;
-  checkWinner();
-});
-
-buttonP2.addEventListener("click", function () {
-  scoreP2++;
-  scoreDisplayP2.innerText = scoreP2;
-  checkWinner();
-});
-
-reset.addEventListener("click", function () {
-  resetScoreKeeper();
-});
-
-games.addEventListener("change", function () {
-  winningScore = parseInt(this.value);
-  resetScoreKeeper();
-});
-
-function checkWinner() {
-  if (scoreP1 === winningScore || scoreP2 === winningScore) {
-    if (scoreP1 > scoreP2) {
-      scoreDisplayP1.classList.add("has-text-success");
-      scoreDisplayP2.classList.add("has-text-danger");
-      buttonP1.disabled = true;
-      buttonP2.disabled = true;
-    } else if (scoreP2 > scoreP1) {
-      scoreDisplayP1.classList.add("has-text-danger");
-      scoreDisplayP2.classList.add("has-text-success");
-      buttonP1.disabled = true;
-      buttonP2.disabled = true;
+function updateScores(player, opponent) {
+  if (!isGameOver) {
+    player.score++;
+    player.scoreDisplay.innerText = player.score;
+    if (player.score === winningScore) {
+      isGameOver = true;
+      player.scoreDisplay.classList.add("has-text-success");
+      opponent.scoreDisplay.classList.add("has-text-danger");
+      player.button.disabled = true;
+      opponent.button.disabled = true;
     }
   }
 }
 
-function resetClasses(scoreDisplay) {
-  scoreDisplay.classList.remove("has-text-success", "has-text-danger");
+function resetPlayers(players) {
+  for (player of players) {
+    player.score = 0;
+    player.scoreDisplay.innerText = 0;
+    player.button.disabled = false;
+    player.scoreDisplay.classList.remove("has-text-success", "has-text-danger");
+  }
+
+  isGameOver = false;
 }
 
-function resetScoreKeeper() {
-  scoreP1 = 0;
-  scoreP2 = 0;
-  scoreDisplayP1.innerText = scoreP1;
-  scoreDisplayP2.innerText = scoreP2;
-  buttonP1.disabled = false;
-  buttonP2.disabled = false;
-  resetClasses(scoreDisplayP1);
-  resetClasses(scoreDisplayP2);
-}
+gamesSelector.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  resetPlayers([p1, p2]);
+});
+
+p1.button.addEventListener("click", function () {
+  updateScores(p1, p2);
+});
+
+p2.button.addEventListener("click", function () {
+  updateScores(p2, p1);
+});
+
+reset.addEventListener("click", function () {
+  resetPlayers([p1, p2]);
+});
