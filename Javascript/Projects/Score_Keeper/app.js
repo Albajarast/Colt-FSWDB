d21940e80e8a1c4259cdc175342a3cb655b5459b
David López Albajara <dlalbajara@gmail.com>
2020-11-09 12:26:35 +0100

    Add logic for modal

--- Javascript/Projects/Score_Keeper/app.js
+++ Javascript/Projects/Score_Keeper/app.js
@@ -2,16 +2,20 @@ const p1 = {
   score: 0,
   button: document.querySelector("#buttonP1"),
   scoreDisplay: document.querySelector("#scoreP1"),
+  modal: document.querySelector("#modalP1"),
 };
 
 const p2 = {
   score: 0,
   button: document.querySelector("#buttonP2"),
   scoreDisplay: document.querySelector("#scoreP2"),
+  modal: document.querySelector("#modalP2"),
 };
 
 const reset = document.querySelector("#reset");
 const gamesSelector = document.querySelector("#games");
+const modal = document.querySelector("#modal");
+const modalP2 = document.querySelector("#modalP2");
 
 let winningScore = parseInt(gamesSelector.value);
 let isGameOver = false;
@@ -26,6 +30,8 @@ function updateScores(player, opponent) {
       opponent.scoreDisplay.classList.add("has-text-danger");
       player.button.disabled = true;
       opponent.button.disabled = true;
+      player.modal.classList.remove("is-hidden");
+      modal.classList.add("is-active");
     }
   }
 }
@@ -36,6 +42,7 @@ function resetPlayers(players) {
     player.scoreDisplay.innerText = 0;
     player.button.disabled = false;
     player.scoreDisplay.classList.remove("has-text-success", "has-text-danger");
+    player.modal.classList.add("is-hidden");
   }
 
   isGameOver = false;
@@ -56,4 +63,9 @@ p2.button.addEventListener("click", function () {
 
 reset.addEventListener("click", function () {
   resetPlayers([p1, p2]);
+  modal.classList.remove("is-active");
+});
+
+modal.addEventListener("click", function () {
+  modal.classList.remove("is-active");
 });
