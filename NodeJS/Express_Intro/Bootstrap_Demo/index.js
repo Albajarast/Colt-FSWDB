ec94ddb0a7c31d0b77549fd75d499f47ac277c08
David López Albajara <dlalbajara@gmail.com>
2020-11-24 14:17:39 +0100

    Duplicated Templating_Demo folder and renamed to Bootstrap_Demo

---
+++ NodeJS/Express_Intro/Bootstrap_Demo/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const data = require("./data.json");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views")); //sets the views directory to the relative path where the index.js is executed
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/rand", (req, res) => {
+  const num = Math.floor(Math.random() * 100 + 1);
+  res.render("rand", { random: num });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  const subredditData = data[subreddit];
+  console.log(subredditData);
+  if (subredditData) {
+    res.render("subreddit", { ...subredditData });
+  } else {
+    res.render("notFound", { subreddit });
+  }
+});
+
+app.get("/cats", (req, res) => {
+  const cats = ["Blue", "Rocket", "Monty", "Stephanie", "Winston"];
+  res.render("cats", { cats });
+});
+
+app.listen(3000, () => {
+  console.log("App running and listening on PORT 3000");
+});
