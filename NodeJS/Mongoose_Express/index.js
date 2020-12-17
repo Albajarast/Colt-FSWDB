7c35458853502c8b1b818036fadff1fa4c335c2d
David López Albajara <dlalbajara@gmail.com>
2020-12-17 16:30:36 +0100

    Add: delete route and funcitonality

--- NodeJS/Mongoose_Express/index.js
+++ NodeJS/Mongoose_Express/index.js
@@ -66,6 +66,12 @@ app.put("/products/:id", async (req, res) => {
   res.redirect(`/products/${product._id}`);
 });
 
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
+
 app.listen(3000, () => {
   console.log("App running and listening on PORT 3000");
 });
