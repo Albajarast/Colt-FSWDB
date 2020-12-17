33e557e2dd0f88021b10026cc242dedfaba84a43
David López Albajara <dlalbajara@gmail.com>
2020-12-17 15:53:00 +0100

    Add: a GET and PUT routes to edit an existent product using method-override

--- NodeJS/Mongoose_Express/index.js
+++ NodeJS/Mongoose_Express/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const Product = require("./models/product");
 
+const categories = ["fruit", "vegetable", "dairy"];
+
+// MongoDB connection using mongoose
 mongoose
   .connect("mongodb://localhost:27017/farmStand", {
     useNewUrlParser: true,
@@ -18,17 +22,20 @@ mongoose
     console.log(err);
   });
 
+// Express APP Middleware
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
+// Routes
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("products/index", { products });
 });
 
 app.get("/products/new", (req, res) => {
-  res.render("products/new");
+  res.render("products/new", { categories });
 });
 
 app.post("/products", async (req, res) => {
@@ -42,6 +49,21 @@ app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product, categories });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
 });
 
 app.listen(3000, () => {
