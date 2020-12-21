e5629dc0fa729ac569f5a528961a0277cae46eff
David López Albajara <dlalbajara@gmail.com>
2020-12-21 15:30:04 +0100

    Fix: name of campground mongoDB model

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const Campground = require("./models/campgrounds");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
