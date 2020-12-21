6ddbeb3151653e5207db1c15fdc54c2d5ac76534
David López Albajara <dlalbajara@gmail.com>
2020-12-21 13:57:38 +0100

    Add: delete route to delete products from DB

--- NodeJS/Mongoose_Express/index.js
+++ NodeJS/Mongoose_Express/index.js
@@ -30,8 +30,14 @@ app.use(methodOverride("_method"));
 
 // Routes
 app.get("/products", async (req, res) => {
-  const products = await Product.find({});
-  res.render("products/index", { products });
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/index", { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render("products/index", { products, category: "All" });
+  }
 });
 
 app.get("/products/new", (req, res) => {
