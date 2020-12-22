f63c00ebd8e2fa03e617a84173882079ea3cbcbb
David López Albajara <dlalbajara@gmail.com>
2020-12-22 13:28:45 +0100

    Add: image, price and description to list on the details

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const newCamp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab eos error nihil autem nam beatae voluptate eius quod itaque velit officia perspiciatis assumenda atque consequatur aspernatur eligendi, distinctio enim ea.",
+      price,
     });
     await newCamp.save();
   }
