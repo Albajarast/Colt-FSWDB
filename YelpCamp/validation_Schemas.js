b22680d87b777f78c05606b416f3a57f012afda8
David López Albajara <dlalbajara@gmail.com>
2020-12-26 14:01:05 +0100

    Add: validation schema for reviews data submissions through form

--- YelpCamp/validation_Schemas.js
+++ YelpCamp/validation_Schemas.js
@@ -9,3 +9,10 @@ module.exports.campgroundVSchema = Joi.object({
     description: Joi.string().required(),
   }).required(),
 });
+
+module.exports.reviewVSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(0).max(5),
+    body: Joi.string().required(),
+  }).required(),
+});
