01589f61c2bb0ad3ad01ed16b18b5a0503f98667
David López Albajara <dlalbajara@gmail.com>
2020-12-26 14:03:57 +0100

    Add: POST route for campground's review form submissions and server side validations

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
-const { campgroundVSchema } = require("./validation_Schemas");
+const { campgroundVSchema, reviewVSchema } = require("./validation_Schemas");
+const Review = require("./models/review");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -27,6 +28,17 @@ app.set("views", path.join(__dirname, "views"));
 
 const validateCampground = (req, res, next) => {
   const { error } = campgroundVSchema.validate(req.body);
+
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(", ");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewVSchema.validate(req.body);
 
   if (error) {
     const msg = error.details.map((el) => el.message).join(", ");
@@ -103,6 +115,19 @@ app.delete(
     const { id } = req.params;
     const campground = await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+  })
+);
+
+app.post(
+  "/campgrounds/:id/reviews",
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
