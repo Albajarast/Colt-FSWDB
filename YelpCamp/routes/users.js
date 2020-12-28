3610c4de8ccc88bf92aa7965036f227823656c33
David López Albajara <dlalbajara@gmail.com>
2020-12-28 12:41:55 +0100

    Add: when a user registers it logs in and login route for already registered users

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -10,21 +10,51 @@ router.get("/register", (req, res) => {
 
 router.post(
   "/register",
-  catchAsync(async (req, res) => {
+  catchAsync(async (req, res, next) => {
     try {
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
-      req.flash(
-        "success",
-        "You have successfully registerd. Welcome to Yelp Camp!"
-      );
-      res.redirect("/campgrounds");
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash(
+          "success",
+          "You have successfully registerd. Welcome to Yelp Camp!"
+        );
+        res.redirect("/campgrounds");
+      });
     } catch (err) {
       req.flash("error", err.message);
       res.redirect("/register");
     }
   })
 );
+
+router.get("/login", (req, res) => {
+  res.render("user/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back");
+    res.redirect("/campgrounds");
+  }
+);
+
+router.get("/logout", (req, res) => {
+  const { username } = req.user;
+  // Capitalize the first letter of the username
+  const showUser = username.replace(/^\w/, (c) => c.toUpperCase());
+  req.logOut();
+  req.flash("success", `Goodbye ${showUser}!`);
+  res.redirect("/campgrounds");
+});
 
 module.exports = router;
