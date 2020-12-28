146a4661809e651254626bb8e51f30fd6dd03b29
David López Albajara <dlalbajara@gmail.com>
2020-12-28 15:26:46 +0100

    Add: isAuthor middleware to check authorisation on some routes

--- YelpCamp/middleware.js
+++ YelpCamp/middleware.js
@@ -1,8 +1,20 @@
+const Campground = require("./models/campgrounds");
+
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl;
     req.flash("error", "you must be signed in");
     return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findById();
+  if (!campground.author.equals("req.user._id")) {
+    req.flash("error", "You do not have permission to do that!");
+    return res.redirect(`/campgrounds/%${id}`);
   }
   next();
 };
