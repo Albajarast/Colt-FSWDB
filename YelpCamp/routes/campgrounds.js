d276468e4e6af607461e97c6e85fdddfc97ca8ac
David López Albajara <dlalbajara@gmail.com>
2020-12-29 10:55:27 +0100

    Add: middleware to upload image files to cloudinary

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const campgroundController = require("../controllers/campgroundController");
+const multer = require("multer");
+const { storage } = require("../cloudinary/index");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(catchAsync(campgroundController.index))
   .post(
     isLoggedIn,
+    upload.array("image"),
     validateCampground,
     catchAsync(campgroundController.create)
   );
