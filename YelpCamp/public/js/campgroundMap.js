5c9266aa5b441c622ccc7ad2acad5ba4585132a3
David López Albajara <dlalbajara@gmail.com>
2021-01-01 15:01:04 +0100

    Add: new map style and pop up

--- YelpCamp/public/js/campgroundMap.js
+++ YelpCamp/public/js/campgroundMap.js
@@ -3,9 +3,16 @@ const geodata = campground.geodata;
 
 var map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+  style: "mapbox://styles/mapbox/light-v10", // stylesheet location
   center: geodata.coordinates, // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat(geodata.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(geodata.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h6>${campground.title}</h6><p>${campground.location}</p>`
+    )
+  )
+  .addTo(map);
